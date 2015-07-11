5c4e02eb8a1b16af3dfdca616c947db434c908f0
Alex R <alexei.rudenko@gmail.com>
2015-07-12 00:55:39 +0200

    Added Opera

--- chrome/index.js
+++ chrome/index.js
@@ -1,23 +1,26 @@
-var onTabChange = (tab) => {
-    var host = tab.url.split("://")[1],
-        tabId = tab.id;
-    
-    if(/^(github|stackoverflow|gitter|tips\.60devs)/.test(host))
-        chrome.pageAction.show(tabId);
-    else
-        chrome.pageAction.hide(tabId);
+/* globals chrome */
+
+var onTabChange = function(tab) {
+  var host = tab.url.split('://')[1]
+  var tabId = tab.id;
+
+  if (/^(github|stackoverflow|gitter|tips\.60devs)/.test(host)) {
+    chrome.pageAction.show(tabId);
+  } else {
+    chrome.pageAction.hide(tabId);
+  }
 }
 
-chrome.tabs.onActivated.addListener((activeInfo) => {
-    chrome.tabs.get(activeInfo.tabId, onTabChange);
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, onTabChange);
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changedInfo, tab) => {
-    onTabChange(tab);
+chrome.tabs.onUpdated.addListener(function(tabId, changedInfo, tab) {
+  onTabChange(tab);
 });
 
-chrome.pageAction.onClicked.addListener((tab) => {
-    chrome.tabs.sendMessage(tab.id, {
-        action: "pageActionClick"
-    });
+chrome.pageAction.onClicked.addListener(function(tab) {
+  chrome.tabs.sendMessage(tab.id, {
+    action: 'pageActionClick'
+  });
 });
